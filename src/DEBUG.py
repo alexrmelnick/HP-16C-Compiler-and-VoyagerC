4005891854bc2e4254a8041d2befbb264bf800fc
alexrmelnick <melnick@bu.edu>
2024-07-22 14:01:02 +0300

    Improved CLI

--- src/DEBUG.py
+++ src/DEBUG.py
@@ -1,4 +1,5 @@
-# Enable to print debug messages
+# Enable to print DEVELOPMENT debug messages
+# These may get removed or replaced with a logger anyways
 DEBUG = False
 is_valid_float_DEBUG = False
 is_number_DEBUG = False
--- src/Jovial_Assembler.py
+++ src/Jovial_Assembler.py
@@ -100,58 +100,68 @@ def parse_interactive(calculator_state):
     print("Welcome to the Jovial Assembler - the first and only assembler for the HP-16C calculator!")
     
     print("Please enter the name or path of the .jov file you would like to assemble.")
-    input_file_name = input("File name: ")
+    input_file_name = input("File name: ").strip()
     while not input_file_name.endswith(".jov"):
         print("Invalid file type. Please enter a .jov file.")
         input_file_name = input("File name: ")
     
-    print("Please enter the name of the output file you would like to create.")
-    output_file_name = input("Output file name: ")
-    
-    print("Would you like to output a .16c file for the HP-16C simulator or a printable .pdf?")
-    output_mode = input("Output mode (16c/pdf): ")
-    while(output_mode != "16c" and output_mode != "pdf"):
-        print("Invalid output mode. Please enter either '16c' or 'pdf'.")
-        output_mode = input("Output mode (16c/pdf): ")
+    print("Please enter the name of the output file with the desired extension format (.pdf/.16c).")
+    temp = input("Output file name: ").strip()
+    # Extract file extension and file name
+    output_mode = temp[-4:].lower() if len(temp) > 4 else ""
+    output_file_name = temp[:-4] if len(temp) > 4 else ""
+    while output_mode not in [".16c", ".pdf"]:
+        print("Invalid output file name. Please enter either a file with a '.16c' or '.pdf' extension.")
+        temp = input("Output file name: ").strip()
+        output_mode = temp[-4:].lower() if len(temp) > 4 else ""
+        output_file_name = temp[:-4] if len(temp) > 4 else ""
+    output_mode = output_mode[1:]  # trim the '.' from output mode
+    print(f"Output mode determined: {output_mode}")
+    print(f"Output file name: {output_file_name}")
 
     print("Please enter the initial settings for the calculator.")
 
-    print("Enter the sign mode (0 = Unsigned, 1 = 1's complement, 2 = 2's complement).")
+    print("Enter the sign mode (0 = Unsigned, 1 = 1's complement, 2 = 2's complement, 3 = floating point).")
     print("If you are unsure, enter 0 for unsigned mode or 2 if you want to use signed numbers.")
     # Checking if sign_mode input is a number
     while True:
         try:
-            sign_mode = int(input("Sign mode (0 = Unsigned, 1 = 1's complement, 2 = 2's complement): "))
-            if 0 <= sign_mode <= 2:
+            sign_mode = int(input("Sign mode (0 = Unsigned, 1 = 1's complement, 2 = 2's complement, 3 = floating point): "))
+            if 0 <= sign_mode <= 3:
                 break  # Exit loop if input is a valid number within range
             else:
-                print("Invalid sign mode. Please enter a valid number (0, 1, or 2).")
+                print("Invalid sign mode. Please enter a valid number (0, 1, 2, or 3).")
         except ValueError:
             print("Please enter a number.")
 
-    print("Enter the word size (number of bits in a word) between 4 bits and 64 bits.")
-    print("If you are unsure, enter 16 for the standard word size. It has a good balance of register size and memory usage.")
-    while True:
-        try:
-            word_size = int(input("Word size (between 4 bits and 64 bits): "))
-            if 4 <= word_size <= 64:
-                break  # Exit loop if input is a valid number within range
-            else:
-                print("Invalid word size. Please enter a number between 4 and 64.")
-        except ValueError:
-            print("Please enter a number.")
+    if sign_mode == 3:
+        # Word size is automatically 56 bits and base 10
+        word_size = 56
+        base = 10
+    else:
+        print("Enter the word size (number of bits in a word) between 4 bits and 64 bits.")
+        print("If you are unsure, enter 16 for the standard word size. It has a good balance of register size and memory usage.")
+        while True:
+            try:
+                word_size = int(input("Word size (between 4 bits and 64 bits): "))
+                if 4 <= word_size <= 64:
+                    break  # Exit loop if input is a valid number within range
+                else:
+                    print("Invalid word size. Please enter a number between 4 and 64.")
+            except ValueError:
+                print("Please enter a number.")
 
-    print("Enter the starting base of the number being entered (2 = binary, 8 = octal, 10 = decimal, 16 = hexadecimal).")
-    print("If you are unsure, enter 10 for decimal. It is the most common base for entering numbers.")
-    while True:
-        try:
-            base = int(input("Base: "))
-            if base in [2, 8, 10, 16]:
-                break  # Exit loop if input is a valid number within the specified options
-            else:
-                print("Invalid base. Please enter the base of the number being entered (2 = binary, 8 = octal, 10 = decimal, 16 = hexadecimal).")
-        except ValueError:
-            print("Please enter a number.")
+        print("Enter the starting base of the number being entered (2 = binary, 8 = octal, 10 = decimal, 16 = hexadecimal).")
+        print("If you are unsure, enter 10 for decimal. It is the most common base for entering numbers.")
+        while True:
+            try:
+                base = int(input("Base: "))
+                if base in [2, 8, 10, 16]:
+                    break  # Exit loop if input is a valid number within the specified options
+                else:
+                    print("Invalid base. Please enter the base of the number being entered (2 = binary, 8 = octal, 10 = decimal, 16 = hexadecimal).")
+            except ValueError:
+                print("Please enter a number.")
 
     # Set the calculator state to the user's input
     calculator_state.sign_mode = sign_mode
@@ -163,18 +173,31 @@ def parse_interactive(calculator_state):
 
 
 def parse_cli(argv, calculator_state):
-    # Check if the user has entered the correct number of arguments
-    if len(argv) != 7:
-        print("Usage: python Jovial_Assembler.py <input filename> <output file name (no extension)> <output mode (16c/pdf)> <sign mode (0/1/2/3)> <word size (4-64)> <base (2/8/10/16)>")
-        sys.exit(1)
-    
     # Parse the command line arguments
-    input_file_name = argv[1]
-    output_file_name = argv[2]
-    output_mode = argv[3]
-    sign_mode = int(argv[4])
-    word_size = int(argv[5])
-    base = int(argv[6])
+    if len(argv) == 3:
+        # Default modes
+        sign_mode = 2
+        word_size = 16
+        base = 10
+
+        input_file_name = argv[1]
+        output_file_name = argv[2][:-4] if len(argv[2]) > 4 else ""
+        output_mode = argv[2][-3:] if len(argv[2]) > 3 else ""
+    elif len(argv) == 6:
+        sign_mode = int(argv[3])
+        word_size = int(argv[4])
+        base = int(argv[5])
+
+        input_file_name = argv[1]
+        output_file_name = argv[2][:-4] if len(argv[2]) > 4 else ""
+        output_mode = argv[2][-3:] if len(argv[2]) > 3 else ""
+    else: 
+        # User entered the wrong number of args
+        print("Usages:")
+        print("python Jovial_Assembler.py [interactive mode - recommended]")
+        print("python Jovial_Assembler.py <input file (.jov)> <output file (.16c/.pdf)> [assumes  2's complement, 16-bit, decimal]")
+        print("python Jovial_Assembler.py <input file (.jov)> <output file (.16c/.pdf)> <sign mode (0/1/2/3)> <word size (4-64)> <base (2/8/10/16)>")
+        sys.exit(1)
 
     # Check if arguments are valid
     if(not input_file_name.endswith(".jov")):
@@ -187,8 +210,8 @@ def parse_cli(argv, calculator_state):
         print("Invalid sign mode. Please use the sign mode (0 = Unsigned, 1 = 1's complement, 2 = 2's complement, 3 = floating point).")
         sys.exit(1)
     if(sign_mode == 3 and word_size != 56):
-        print("Invalid word size. Floating point numbers must be 56 bits.")
-        sys.exit(1)
+        # Just set the word size to 56 bits
+        word_size = 56
     if(word_size < 4 or word_size > 64):
         print("Invalid word size. Please use a word size between 4 and 64 bits.")
         sys.exit(1)
